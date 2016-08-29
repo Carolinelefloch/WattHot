2fedb21c223761690af9d1d07c37e6e2f6d26751
dawei wang <daw068@eng.ucsd.edu>
2016-08-29 11:02:55 -0700

    convvert the indetation to space

--- lib/Description.py
+++ lib/Description.py
@@ -2,7 +2,7 @@
 Created on Aug 16 2016
 @author: David
 
-Description module:		Return the Hash of the rate name and its description for the given utility name
+Description module:     Return the Hash of the rate name and its description for the given utility name
 
 
 '''
@@ -10,43 +10,40 @@ Description module:		Return the Hash of the rate name and its description for th
 import sqlite3
 
 class Description:
-	def __init__(self, path1):
-		# Connect the EV and Household model databese
-		conn1 = sqlite3.connect(path1)
-		self.c1 = conn1.cursor()
+    def __init__(self, path1):
+        # Connect the EV and Household model databese
+        conn1 = sqlite3.connect(path1)
+        self.c1 = conn1.cursor()
 
-	def Get_Descrip(self,utilityName):
-		command=(
-					'''
-					SELECT Description from Rate_Description
-					INNER JOIN Utility_Rate_Name
-					ON Rate_Description.Rate_id == Utility_Rate_Name.Rate_id
-					WHERE Utility_Name=? 
-					'''
-					)
-		Description=self.c1.execute(command,(utilityName,)).fetchall()
+    def Get_Descrip(self,utilityName):
+        command=(
+                    '''
+                    SELECT Description from Rate_Description
+                    INNER JOIN Utility_Rate_Name
+                    ON Rate_Description.Rate_id == Utility_Rate_Name.Rate_id
+                    WHERE Utility_Name=? 
+                    '''
+                    )
+        Description=self.c1.execute(command,(utilityName,)).fetchall()
 
-		command=(
-					'''
-					SELECT Eligibility from Rate_Description
-					INNER JOIN Utility_Rate_Name
-					ON Rate_Description.Rate_id == Utility_Rate_Name.Rate_id
-					WHERE Utility_Name=? 
-					'''
-					)
-		elig=self.c1.execute(command,(utilityName,)).fetchall()
+        command=(
+                    '''
+                    SELECT Eligibility from Rate_Description
+                    INNER JOIN Utility_Rate_Name
+                    ON Rate_Description.Rate_id == Utility_Rate_Name.Rate_id
+                    WHERE Utility_Name=? 
+                    '''
+                    )
+        elig=self.c1.execute(command,(utilityName,)).fetchall()
 
-		command=(
-					'''
-					SELECT Rate_Name	 from Utility_Rate_Name
-					WHERE Utility_Name=? 
-					
-					'''
-				)
-		Rate_Names=self.c1.execute(command,(utilityName,)).fetchall()
-		
-		result={'Rate Name': Rate_Names,'Description':Description,'Eligibility':elig}
-		return result
-
-		
-		
+        command=(
+                    '''
+                    SELECT Rate_Name     from Utility_Rate_Name
+                    WHERE Utility_Name=? 
+                    
+                    '''
+                )
+        Rate_Names=self.c1.execute(command,(utilityName,)).fetchall()
+        
+        result={'Rate Name': Rate_Names,'Description':Description,'Eligibility':elig}
+        return result
